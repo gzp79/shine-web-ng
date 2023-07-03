b85ff4d14e18fac65b33680976097ca758434ea7
gzp <->
2023-07-03 18:05:57 +0200

    git action tests

--- src/app/core/config.ts
+++ src/app/core/config.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
   api_url: string = "https://cloud.scytta.com"
-  web_url: string = "https://local.scytta.com"
+  //web_url: string = "https://local.scytta.com"
+  web_url: string = "https://www.scytta.com"
 }
